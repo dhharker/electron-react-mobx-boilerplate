a6a5ceb172a503113b976df130f2985a2d84b1eb
David Harker <git@dhharker.net>
2018-05-14 12:51:43 +0100

    (ref)

--- src/app/widgets/EntriesCount.js
+++ src/app/widgets/EntriesCount.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default class EntriesCount extends React.Component {
   render() {
+    const { numEntries } = this.props;
     return (
-      <div>There are {this.props.numEntries} entries.</div>
+      <div>There are {numEntries} entries.</div>
     );
   }
 }
